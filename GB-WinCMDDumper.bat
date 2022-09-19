13563b843a9d4f89473d513395f6a6762c8dd52d
glob-bruh <34488272+glob-bruh@users.noreply.github.com>
2022-09-18 19:19:18 -0600

    Remove Malwarebytes Log Collection

--- GB-WinCMDDumper.bat
+++ GB-WinCMDDumper.bat
@@ -1,31 +1,13 @@
-::
-::    WINDOWS CMD DUMPER
-::
-::    This program is free software: you can redistribute it and/or modify
-::    it under the terms of the GNU General Public License as published by
-::    the Free Software Foundation, either version 3 of the License, or
-::    (at your option) any later version.
-::
-::    This program is distributed in the hope that it will be useful,
-::    but WITHOUT ANY WARRANTY; without even the implied warranty of
-::    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-::    GNU General Public License for more details.
-::
-::    You should have received a copy of the GNU General Public License
-::    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-::
+
+:: WINDOWS CMD DUMPER
 
 @echo off
 
 echo ******************************************************************
 echo * Windows CMD Dumper                                             *
-echo * Copyright (c) 2022 GlobBruh                                    *
+echo * By: GlobBruh                                                   *
 echo *                                                                *
-echo * This program comes with ABSOLUTELY NO WARRANTY.                *
-echo * This is free software, and you are welcome to redistribute it  *
-echo * under certain conditions.                                      *
-echo *                                                                *
-echo * Instructions available in the README.md file.                  *
+echo * Instructions available in the README.md file or on GitHub.     *
 echo *                                                                *
 echo * https://github.com/glob-bruh/WindowsCMDDumper                  *
 echo ******************************************************************
@@ -70,10 +52,6 @@ echo Netstat Detailed...
 netstat -a -q -b -o > .\GlobBruhDUMP\NetstatDETAILED.txt
 echo Netstat Stats...
 netstat -s > .\GlobBruhDUMP\NetstatSTATS.txt
-
-echo Copy MalwareBytes Logs...
-mkdir .\GlobBruhDUMP\MalwarebyesLogs
-copy C:\ProgramData\Malwarebytes\MBAMService\ScanResults\ .\GlobBruhDUMP\MalwarebyesLogs
 
 echo Compress Folder...
 powershell Compress-Archive ./GlobBruhDUMP/ ./GLOBBRUH-DUMP.zip
